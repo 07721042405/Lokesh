4a718f0d37123b42dc5410dd815c14437e2853be
lokesh <lokeshdhawad@gmail.com>
2022-07-03 22:27:42 +0530

    first commit

---
+++ src/gitx/Calculator.java
@@ -0,0 +1,16 @@
+package gitx;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=10;
+		b=10;
+		c=a+b;
+		System.out.println(c);
+				
+
+	}
+
+}
